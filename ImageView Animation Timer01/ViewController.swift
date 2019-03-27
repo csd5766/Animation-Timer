03d37fd944e28cda306ec8b8f0d2b48cda8396c2
D7702_09 <d7702_09@d7702-09ui-imac.local>
2019-03-27 16:30:59 +0900

    애니메이션 타이머 구현

--- ImageView Animation Timer01/ViewController.swift
+++ ImageView Animation Timer01/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var aiview: UIImageView!
+    var count = 1
+   // var isAnimation = false
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        aiview.image = UIImage(named: "Image1")
+        
+        
     }
 
+    @IBAction func playBtn(_ sender: Any) {
+         //타이머 호출
+        myTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(doAnymation), userInfo: nil, repeats: true)
+    
+      
+        
+    }
+    
+    @IBAction func pauseBtn(_ sender: Any) {
+    myTimer.invalidate()
+    }
+    
+    @IBAction func stopBtn(_ sender: Any) {
+        myTimer.invalidate()
+    }
+    @objc func doAnymation(){
+    if (count == 5) {
+       count=1
+    }
+    else {
+    count+=1
+    }
+    aiview.image = UIImage(named: "Image\(count)")
+         countLabel.text = String(count)
+    }
+    
+    
 
 }
 
